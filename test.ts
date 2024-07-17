9af39eb7a937a27d5e1b59a28bd3a0bd9f833fee
Antonio Stoilkov <hello@astoilkov.com>
2024-07-17 11:16:45 +0300

    ✅ try make tests work

--- test.ts
+++ test.ts
@@ -1,14 +1,33 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import createDeepMemo from "./src/createDeepMemo";
 import createDeepSignal from "./src/createDeepSignal";
+import { createRenderEffect, createRoot } from "solid-js";
 
 // for some reason, Solid reactivity doesn't work in tests
 describe("create-deep", () => {
-    test("createDeepSignal()", () => {
-        createDeepSignal(() => [1, 2, 3]);
+    test.only("createDeepSignal()", () => {
+        createTestRoot(() => {
+            const [, setValue] = createDeepSignal([1, 2, 3]);
+
+            let calls = 0
+            createRenderEffect(() => {
+                calls += 1
+            })
+
+            setValue([1, 2, 3, 4])
+
+            // expect(calls).toBe(0)
+        })
     });
 
     test("createDeepMemo()", () => {
         createDeepMemo(() => [1, 2, 3]);
     });
 });
+
+function createTestRoot(fn: () => void): void {
+    createRoot((dispose) => {
+        fn();
+        dispose()
+    });
+}
