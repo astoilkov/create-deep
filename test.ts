e0c643828e106e626474c2ee2277cfad514934ce
Antonio Stoilkov <hello@astoilkov.com>
2024-07-17 11:17:53 +0300

    🐜 forgot an unused import

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { describe, test } from "vitest";
 import createDeepMemo from "./src/createDeepMemo";
 import createDeepSignal from "./src/createDeepSignal";
 import { createRenderEffect, createRoot } from "solid-js";
